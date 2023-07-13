d1bdccdf629de994caa8932a9a1490d5650fb629
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-13 08:03:44 +0800

    feat: read db connection from env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,22 @@
+import { config } from 'dotenv';
 import { Config } from "drizzle-kit";
+import { parseEnv } from "znv";
+import { z } from "zod";
+
+config();
+
+const { DB_URL, DB_AUTH_TOKEN } = parseEnv(process.env, {
+  DB_URL: z.string().url().default("file:./local.db"),
+  DB_AUTH_TOKEN: z.string().optional().describe("Auth token for the database"),
+});
 
 export default {
   schema: "./schema.ts",
   out: "./src/migrations",
   driver: "turso",
   dbCredentials: {
-    url: 'file:./local.db',
+    url: DB_URL,
+    authToken: DB_AUTH_TOKEN,
   },
   verbose: true,
   strict: true
